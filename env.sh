37ef33c1fa3139e8f4a2526baa3216f6d9eedbe9
hta <offreitas@outlook.com>
2024-03-20 11:31:49 -0300

    Dev branch

--- env.sh
+++ env.sh
@@ -8,10 +8,11 @@ export CC_RISCV64=$BIN_DIR/riscv64-linux-
 export SPDM_DIR=$WORKSPACE/libspdm
 export SPDM_BUILD_DIR=$SPDM_DIR/build_uboot
 export PATH="$PATH:$WORKSPACE/buildroot/output/host/bin"
+export OPENSBI=$WORKSPACE/opensbi/build/platform/generic/firmware/fw_dynamic.bin
+export CROSS_COMPILE=$CC_RISCV64
 
-if [ `uname -m` == "x86_64" ]
-then
-    export HOST_ARCH="x64"
+if [[ $(uname -m) == "x86_64" ]]; then
+	export HOST_ARCH="x64"
 else
-    export HOST_ARCH=$(uname -m)
+	export HOST_ARCH=$(uname -m)
 fi
--- run.sh
+++ run.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-opt=`echo $1 | awk '{split($0, a, "-"); print a[2]}'`
+opt=$(echo $1 | awk '{split($0, a, "-"); print a[2]}')
 
-if [ $opt == "hd" ]
-then
-  $WORKSPACE/qemu/build/riscv64-softmmu/qemu-system-riscv64 \
-    -smp 2 \
-    -nographic \
-    -m 8G \
-    -M virt \
-    -bios $WORKSPACE/opensbi/build/platform/generic/firmware/fw_payload.elf \
-    -drive file=$WORKSPACE/disk.img,format=raw,id=hd0 \
-    -device virtio-blk-device,drive=hd0
-elif [ $opt == "sd" ]
-then
-  $WORKSPACE/qemu/build/riscv64-softmmu/qemu-system-riscv64 \
-    -smp 2 \
-    -nographic \
-    -m 8G \
-    -M virt \
-    -bios $WORKSPACE/opensbi/build/platform/generic/firmware/fw_payload.elf \
-    -drive file=$WORKSPACE/disk.img,format=raw,id=sd1 \
-    -device sd-card,drive=sd1
+if [ $opt == "hd" ]; then
+	$WORKSPACE/qemu/build/riscv64-softmmu/qemu-system-riscv64 \
+		-smp 2 \
+		-nographic \
+		-m 2G \
+		-M virt \
+		-bios $WORKSPACE/opensbi/build/platform/generic/firmware/fw_payload.elf \
+		-drive file=$WORKSPACE/disk.img,format=raw,id=hd0 \
+		-device virtio-blk-device,drive=hd0
+elif [ $opt == "sd" ]; then
+	$WORKSPACE/qemu/build/riscv64-softmmu/qemu-system-riscv64 \
+		-M sifive_u,msel=11 \
+		-smp 5 \
+		-nographic \
+		-m 8G \
+		-bios $WORKSPACE/u-boot/spl/u-boot-spl.bin \
+		-drive file=$WORKSPACE/sdimg/images/sdcard.img,if=sd,format=raw
 fi
